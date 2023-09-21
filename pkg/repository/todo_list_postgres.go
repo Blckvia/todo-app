4ba90156dd1d912b2edafcf7b4810c9e70538bec
blckvia <a.vyatkinn@icloud.com>
2023-09-21 12:13:43 +0300

    test

--- pkg/repository/todo_list_postgres.go
+++ pkg/repository/todo_list_postgres.go
@@ -97,7 +97,7 @@ func (r *TodoListPostgres) Update(userId, listId int, input todo.UpdateListInput
 	args = append(args, listId, userId)
 
 	logrus.Debugf("updateQuery: %s", query)
-	logrus.Debugf("args: %s", args)
+	logrus.Debugf("args: %s", args) 
 
 	_, err := r.db.Exec(query, args...)
 	return err
